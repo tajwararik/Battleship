6312c0ea7d5b68d4704768620f3eda5c5a003db1
tajwararik <tajwar.arik.10@gmail.com>
2024-12-28 20:25:59 +0600

    Added ship class

---
+++ src/modules/ships.js
@@ -0,0 +1,14 @@
+export class Ship {
+  constructor(length) {
+    this.length = length;
+    this.hits = 0;
+  }
+
+  hit() {
+    this.hits++;
+  }
+
+  isSunk() {
+    return this.hits >= this.length;
+  }
+}
