379c1e6f76240d0ae7595690fa2e316186ac80a4
tajwararik <tajwar.arik.10@gmail.com>
2024-12-28 20:25:30 +0600

    Implemented tests for ship class

---
+++ src/modules/ship.test.js
@@ -0,0 +1,32 @@
+import { Ship } from "./ships.js";
+
+describe("Ship class tests", () => {
+  let ship;
+
+  beforeEach(() => {
+    ship = new Ship(3);
+  });
+
+  test("hit() increments the hits property", () => {
+    ship.hit();
+    expect(ship.hits).toBe(1);
+  });
+
+  test("isSunk() return true if all the parts are hit", () => {
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    expect(ship.isSunk()).toBe(true);
+  });
+
+  test("isSunk() return false if all the parts are not hit", () => {
+    ship.hit();
+    expect(ship.isSunk()).toBe(false);
+  });
+
+  test("Ship of length 1 is sunk after 1 hit", () => {
+    const singleLengthShip = new Ship(1);
+    singleLengthShip.hit();
+    expect(singleLengthShip.isSunk()).toBe(true);
+  });
+});
