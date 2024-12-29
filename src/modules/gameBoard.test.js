d1a1e1f634603069b0d5d29bbee5d07c9f178377
tajwararik <tajwar.arik.10@gmail.com>
2024-12-30 05:08:16 +0600

    Created tests for game board

---
+++ src/modules/gameBoard.test.js
@@ -0,0 +1,27 @@
+import { GameBoard } from "./gameBoard.js";
+
+describe("Game board class tests", () => {
+  let gameBoard;
+
+  beforeEach(() => {
+    gameBoard = new GameBoard();
+  });
+
+  test("Checking if game board exists", () => {
+    expect(gameBoard.board).toBeDefined();
+  });
+
+  test("Checking if the board has 10 rows", () => {
+    expect(gameBoard.board.length).toBe(10);
+  });
+
+  test("Checking if the board has 10 columns", () => {
+    gameBoard.board.forEach((x) => expect(x.length).toBe(10));
+  });
+
+  test("Checking if all values of the board is null", () => {
+    gameBoard.board.forEach((x) => {
+      x.forEach((y) => expect(y).toBeNull());
+    });
+  });
+});
