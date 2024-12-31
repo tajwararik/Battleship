493c25fca230e13b5dc861fba954e62d4826bd36
tajwararik <tajwar.arik.10@gmail.com>
2024-12-31 16:40:34 +0600

    Added palceShips method to the game board class

--- src/modules/gameBoard.js
+++ src/modules/gameBoard.js
@@ -4,4 +4,24 @@ export class GameBoard {
       .fill()
       .map(() => Array(10).fill(null));
   }
+
+  placeShips(ship, x, y, direction) {
+    if (direction === "horizontal") {
+      if (y + ship.length >= 10) throw Error("Ship out of bounds");
+
+      for (let i = 0; i < ship.length; i++) {
+        if (this.board[x][y + i]) throw Error("Overlap detected");
+
+        this.board[x][y + i] = ship;
+      }
+    } else if (direction === "vertical") {
+      if (x + ship.length >= 10) throw Error("Ship out of bounds");
+
+      for (let i = 0; i < ship.length; i++) {
+        if (this.board[x + i][y]) throw Error("Overlap detected");
+
+        this.board[x + i][y] = ship;
+      }
+    }
+  }
 }
