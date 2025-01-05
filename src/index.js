7cfe44be06c7d5f35b587e40e843179b0a9a44e4
tajwararik <tajwar.arik.10@gmail.com>
2025-01-05 07:03:46 +0600

    Loaded the game boards

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+import {
+  createPlayerBoard,
+  createComputerBoard,
+} from "./modules/domGameHandle.js";
 import "./styles.css";
+
+document.addEventListener("DOMContentLoaded", () => {
+  createPlayerBoard();
+  createComputerBoard();
+});
