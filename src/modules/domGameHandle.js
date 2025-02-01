f11a19b0506c7942d42e1392b45639da4b118fd4
tajwararik <tajwar.arik.10@gmail.com>
2025-02-02 04:23:02 +0600

    Creatd ships and coordinates

--- src/modules/domGameHandle.js
+++ src/modules/domGameHandle.js
@@ -1,9 +1,28 @@
 import { Game } from "./game.js";
+import { Ship } from "./ships.js";
 
 const playerBoard = document.querySelector("#player-board");
 const computerBoard = document.querySelector("#computer-board");
 
 const game = new Game();
+
+const ships = [new Ship(1), new Ship(2), new Ship(3), new Ship(4), new Ship(5)];
+
+const playerShipsCoordinate = [
+  { x: 2, y: 1, direction: "horizontal" },
+  { x: 0, y: 7, direction: "horizontal" },
+  { x: 2, y: 5, direction: "vertical" },
+  { x: 5, y: 0, direction: "vertical" },
+  { x: 8, y: 4, direction: "horizontal" },
+];
+
+const computerShipsCoordinate = [
+  { x: 3, y: 9, direction: "horizontal" },
+  { x: 9, y: 6, direction: "horizontal" },
+  { x: 6, y: 5, direction: "horizontal" },
+  { x: 6, y: 0, direction: "vertical" },
+  { x: 0, y: 3, direction: "vertical" },
+];
 
 const boardCells = game.player.board.board.length;
 
@@ -12,7 +31,6 @@ export function createPlayerBoard() {
     for (let j = 0; j < boardCells; j++) {
       const div = document.createElement("div");
       div.classList.add("grid");
-      div.setAttribute("id", `${[i, j]}`);
       playerBoard.append(div);
     }
   }
@@ -23,7 +41,6 @@ export function createComputerBoard() {
     for (let j = 0; j < boardCells; j++) {
       const div = document.createElement("div");
       div.classList.add("grid");
-      div.setAttribute("id", `${[i, j]}`);
       computerBoard.append(div);
     }
   }
