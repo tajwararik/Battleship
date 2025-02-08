7de07bfd683bca4f4595b60325950e17f371755b
tajwararik <tajwar.arik.10@gmail.com>
2025-02-08 19:19:23 +0600

    Implemented currentPlayer and switchTurn method

--- src/modules/game.js
+++ src/modules/game.js
@@ -4,5 +4,10 @@ export class Game {
   constructor() {
     this.player = new Player("Player");
     this.computer = new Player("Computer", true);
+    this.currentPlayer = "player";
+  }
+
+  switchTurn() {
+    this.currentPlayer = this.currentPlayer === "player" ? "computer" : "player";
   }
 }
