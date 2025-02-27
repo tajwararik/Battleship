2bba37e97f9da41ad1fdaa89b59de03bebe3922e
tajwararik <tajwar.arik.10@gmail.com>
2025-02-27 17:07:06 +0600

    Imported handleDisplayMessage function

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import {
   createPlayerBoard,
   createComputerBoard,
+  handleDisplayMessage,
 } from "./modules/domGameHandle.js";
 import "./styles.css";
 
 document.addEventListener("DOMContentLoaded", () => {
   createPlayerBoard();
   createComputerBoard();
+  handleDisplayMessage();
 });
